08779b89450a98287697ca5f2f6cdf5de3528e30
kyzz3 <kyzz3@LAPTOP-UND279KI>
2020-11-13 17:47:17 +0900

    ㅇㅇ

--- desert/src/main/java/com/desert/demo/dto/DTOPerQus.java
+++ desert/src/main/java/com/desert/demo/dto/DTOPerQus.java
@@ -1,6 +1,6 @@
 package com.desert.demo.dto;
 
-import java.sql.Timestamp;
+import java.time.LocalDate;
 
 import lombok.Data;
 
@@ -8,13 +8,13 @@ import lombok.Data;
 public class DTOPerQus {
 
 	private int idx;
-	private Timestamp question_date;
+	private LocalDate question_date;
 	private String category;
 	private String reply;
 	private String member_id;
 	private String answer_contents;
 	private String question_contents;
 	private String question_title;
-	private Timestamp reply_date;
+	private LocalDate reply_date;
 
 }
