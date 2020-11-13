6dc97472afb2fb4f5b38059698f40c51d201d314
kyzz3 <kyzz3@LAPTOP-UND279KI>
2020-11-13 15:31:03 +0900

    ㅎㅇ~

--- desert/src/main/java/com/desert/demo/config/DBConfig.java
+++ desert/src/main/java/com/desert/demo/config/DBConfig.java
@@ -36,7 +36,7 @@ public class DBConfig {
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
-        factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mappers/**/*Mapper.xml"));
+        //factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mappers/**/*Mapper.xml"));
         factoryBean.setTypeAliasesPackage("com.board.domain");
         factoryBean.setConfiguration(mybatisConfg());
         return factoryBean.getObject();
@@ -52,5 +52,4 @@ public class DBConfig {
     public org.apache.ibatis.session.Configuration mybatisConfg() {
         return new org.apache.ibatis.session.Configuration();
     }
-
 }
